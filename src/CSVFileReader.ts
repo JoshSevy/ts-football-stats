b62ff6071bd1a3755d0978077bc6f49ea5e7676c
JoshSevy <joshuasevy@outlook.com>
2020-12-14 21:56:24 -0700

    Add type definition for mapping function in csv reader

--- src/CSVFileReader.ts
+++ src/CSVFileReader.ts
@@ -2,6 +2,8 @@ import fs from 'fs';
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
 
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 export class CSVFileReader {
   data: string[][] = [];
 
@@ -19,13 +21,15 @@ export class CSVFileReader {
         }
       )
       .map(
-        (row: string[]): any => {
+        (row: string[]): MatchData => {
           return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4])
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult,
+            row[6]
           ];
         }
       );
