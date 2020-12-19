5186766a66b9ed7df8de9783b8dffcea697dac6c
JoshSevy <joshuasevy@outlook.com>
2020-12-18 19:31:53 -0700

    Updated index.ts and csv reader to be more dynamic, added static methods to classes

--- build/index.js
+++ build/index.js
@@ -1,12 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var CSVFileReader_1 = require("./CSVFileReader");
 var Summary_1 = require("./Summary");
-var csvFileReader = new CSVFileReader_1.CSVFileReader('./data/football.csv');
-var matchReader = new MatchReader_1.MatchReader(csvFileReader);
-matchReader.load();
+var matchReader = MatchReader_1.MatchReader.fromCSV('./data/football.csv');
 var htmlSummary = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
 var consoleSummary = Summary_1.Summary.winsAnalysisWithConsoleReport('Man United');
+matchReader.load();
 htmlSummary.buildAndPrintReport(matchReader.matches);
 consoleSummary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 import { MatchReader } from './MatchReader';
-import { CSVFileReader } from './CSVFileReader';
-
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { HTMLReport } from './reportTargets/HTMLReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
 
-const csvFileReader = new CSVFileReader('./data/football.csv');
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-
+const matchReader = MatchReader.fromCSV('./data/football.csv')
 const htmlSummary = Summary.winsAnalysisWithHtmlReport('Man United');
 const consoleSummary = Summary.winsAnalysisWithConsoleReport('Man United');
 
+matchReader.load();
 htmlSummary.buildAndPrintReport(matchReader.matches);
 consoleSummary.buildAndPrintReport(matchReader.matches);
