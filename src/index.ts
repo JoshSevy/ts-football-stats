c71af24f0954ef34f2f9921da999d8a1b5b378f3
JoshSevy <joshuasevy@outlook.com>
2020-12-18 18:35:18 -0700

    Clean up index.ts to take in new classes to anaylze data

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
 import { CSVFileReader } from './CSVFileReader';
+
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
 const csvFileReader = new CSVFileReader('./data/football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
 
-console.log(`Manchester United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
 
