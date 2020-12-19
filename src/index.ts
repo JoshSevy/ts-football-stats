73e6aeeea3218e5321b001730b39c8e5565a836a
JoshSevy <joshuasevy@outlook.com>
2020-12-18 18:14:34 -0700

    Add match data analyer to project

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,7 @@ import { CSVFileReader } from './CSVFileReader';
 const csvFileReader = new CSVFileReader('./data/football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-let manUnitedWins = 0;
 
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
 
 console.log(`Manchester United won ${manUnitedWins} games`);
 
